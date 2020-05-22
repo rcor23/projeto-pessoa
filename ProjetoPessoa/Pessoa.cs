31693b7f3c3d7c64f8028eb2ff6c1d3ec3370b8b
rcor23 <65785381+rcor23@users.noreply.github.com>
2020-05-22 20:58:33 -0300

    projeto pessoa

---
+++ ProjetoPessoa/Pessoa.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+
+namespace ProjetoPessoa
+{
+    class Pessoa
+    {
+        private string nome;
+        private DateTime dataNascimento;
+        private double altura;
+
+        public string Nome
+        {
+            set
+            {
+                if (value != string.Empty)
+                {
+                    this.nome = value;
+                }
+                else
+                {
+                    throw new Exception("O nome deve ser obrigatoriamente preenchido.");
+                }
+            }
+            get { return this.nome; }
+        }
+
+        public DateTime DataNascimento
+        {
+            set { this.dataNascimento = value; }
+            get { return this.dataNascimento; }
+        }
+
+        public double Altura
+        {
+            set 
+            {
+                if (value >= 1.00)
+                {
+                    this.altura = value;
+                }
+                else
+                {
+                    throw new Exception("A altura deve ser acima de 1m.");
+                }
+            }
+            get { return this.altura; }
+        }
+
+        public string ImprimeDados()
+        {
+            return "Dados da pessoa: \n\nNome: " + 
+                    this.nome + "\nData de Nascimento: " +
+                    this.dataNascimento.ToString("dd/MM/yyyy") + 
+                    "\nAltura: " + this.altura.ToString("0.00") + "m.";
+        }
+
+        public int CalcularIdade()
+        {
+            return DateTime.Now.Year - this.dataNascimento.Year;
+        }
+    }
+}
